cb458a53db32e8275a39f6173a634db69d967d64
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2023-06-09 21:11:06 +0400

    create spring boot project

---
+++ src/test/java/com/lineate/mdzinarishvili/reminderapp/ReminderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lineate.mdzinarishvili.reminderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
