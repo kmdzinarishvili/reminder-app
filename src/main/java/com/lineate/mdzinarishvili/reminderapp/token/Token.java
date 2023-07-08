fe01e902c1bc41072e37564982df0f4eb6097b1b
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2023-07-08 19:02:43 +0400

    fix merge conflicts

---
+++ src/main/java/com/lineate/mdzinarishvili/reminderapp/token/Token.java
@@ -0,0 +1,43 @@
+package com.lineate.mdzinarishvili.reminderapp.token;
+
+import com.lineate.mdzinarishvili.reminderapp.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+//@Entity
+public class Token {
+
+//  @Id
+//  @GeneratedValue
+  public Long id;
+
+//  @Column(unique = true)
+  public String token;
+
+//  @Enumerated(EnumType.STRING)
+  public TokenType tokenType = TokenType.BEARER;
+
+  public boolean revoked;
+
+
+  public boolean expired;
+
+
+//  @ManyToOne(fetch = FetchType.LAZY)
+//  @JoinColumn(name = "user_id")
+  public User user;
+
+  public Token (Long id, String token, boolean revoked, boolean expired){
+    this.id = id;
+    this.token=token;
+    this.revoked = revoked;
+    this.expired = expired;
+  }
+
+}
--- src/main/resources/db/migration/V2__users.sql
+++ src/main/resources/db/migration/V2__users.sql
@@ -5,12 +5,10 @@ CREATE TABLE roles (
   role_id SERIAL PRIMARY KEY,
   role_name VARCHAR(45) NOT NULL
 );
-
-CREATE TABLE roles_users (
-  user_id INT NOT NULL,
-  role_id INT NOT NULL,
-  CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
-  CONSTRAINT role_fk FOREIGN KEY (role_id) REFERENCES roles (role_id)
-);
 INSERT INTO roles (role_name) VALUES ('USER');
 INSERT INTO roles (role_name) VALUES ('ADMIN');
+
+alter table users add column role_id int;
+alter table users add constraint fk_role_id foreign key (role_id) references roles(role_id);
+
+
