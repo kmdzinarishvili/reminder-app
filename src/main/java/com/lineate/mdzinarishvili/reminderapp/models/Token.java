417b483bdc219e5ac8059f2a19838cbc24edb182
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2023-07-08 18:49:13 +0400

    get user with token

--- src/main/java/com/lineate/mdzinarishvili/reminderapp/models/Token.java
+++ src/main/java/com/lineate/mdzinarishvili/reminderapp/models/Token.java
@@ -16,7 +16,6 @@ public class Token {
 
   public String token;
 
-//  @Enumerated(EnumType.STRING)
   public TokenType tokenType = TokenType.BEARER;
 
   public boolean revoked;
--- src/main/java/com/lineate/mdzinarishvili/reminderapp/models/TokenMapper.java
+++ src/main/java/com/lineate/mdzinarishvili/reminderapp/models/TokenMapper.java
@@ -1,5 +1,6 @@
 package com.lineate.mdzinarishvili.reminderapp.models;
 
+import com.lineate.mdzinarishvili.reminderapp.enums.RoleType;
 import com.lineate.mdzinarishvili.reminderapp.models.Token;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -12,12 +13,11 @@ public class TokenMapper  implements RowMapper<Token>{
     public Token mapRow(ResultSet resultSet, int i) throws SQLException {
         return new Token(resultSet.getLong("token_id"),
                 resultSet.getString("token_value"),
-//                TokenType.BEARER,
                 resultSet.getBoolean("revoked"),
-                resultSet.getBoolean("expired")
-//                ,
-//                new User(resultSet.getLong("user_id"),resultSet.getString("username"),
-//                         resultSet.getString("email"),resultSet.getString("password"),Role.ADMIN)
+                resultSet.getBoolean("expired"),
+                new User(resultSet.getLong("user_id"),resultSet.getString("username"),
+                         resultSet.getString("email"),resultSet.getString("password"),
+                         RoleType.valueOf(resultSet.getString("role")))
         );
         }
 
