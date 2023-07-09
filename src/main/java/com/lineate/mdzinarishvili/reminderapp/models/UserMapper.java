f9b60c13abbf7885d1da7aca82835cba0912db05
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2023-07-09 18:02:35 +0400

    code clean-up

--- src/main/java/com/lineate/mdzinarishvili/reminderapp/models/UserMapper.java
+++ src/main/java/com/lineate/mdzinarishvili/reminderapp/models/UserMapper.java
@@ -1,35 +1,19 @@
 package com.lineate.mdzinarishvili.reminderapp.models;
 
-import com.lineate.mdzinarishvili.reminderapp.enums.RecurrenceType;
 import com.lineate.mdzinarishvili.reminderapp.enums.RoleType;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Arrays;
-import java.util.Optional;
 
-import static com.lineate.mdzinarishvili.reminderapp.models.ReminderMapper.valueOfIgnoreCase;
+
 
 public class UserMapper implements RowMapper<User> {
-    public User mapRow(ResultSet resultSet, int i) throws SQLException {
-
+    public User mapRow(final ResultSet resultSet, int i) throws SQLException {
         return new User(resultSet.getLong("user_id"),
                 resultSet.getString("username"),
                 resultSet.getString("email"),
                 resultSet.getString("password"),
-                valueOfIgnoreCase( resultSet.getString("role")).orElse(null));
-    }
-    public static Optional<RoleType> valueOfIgnoreCase(String name) {
-        return Arrays.stream(RoleType.values())
-                .filter(type -> type.name().equalsIgnoreCase(name))
-                .findAny();
+                RoleType.valueOf( resultSet.getString("role")));
     }
 }
-//    private Long id;
-//    private String username;
-//    private String email;
-//    private String password;
-//    private RoleType role;
