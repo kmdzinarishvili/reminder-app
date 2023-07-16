385af56b0793c9026fe553391959bf6d3758b67d
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2023-07-16 22:52:22 +0400

    logs and error message fix

--- src/main/java/com/lineate/mdzinarishvili/reminderapp/config/ApplicationConfig.java
+++ src/main/java/com/lineate/mdzinarishvili/reminderapp/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.lineate.mdzinarishvili.reminderapp.config;
 import com.lineate.mdzinarishvili.reminderapp.dao.UserDaoImpl;
 import com.lineate.mdzinarishvili.reminderapp.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,23 +11,30 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class ApplicationConfig {
   private final UserDaoImpl repository;
 
   @Bean
   public UserDetailsService userDetailsService() {
-    return username -> repository.selectUserByUsername(username)
-        .orElseThrow(() -> new NotFoundException("Username Not found"));
+    return username -> {
+      log.info("Selecting user by username {}", username);
+      return repository.selectUserByUsername(username)
+          .orElseThrow(() -> {
+            log.error("Username not found {}", username);
+            return new NotFoundException(String.format("Username Not found %s", username));
+          });
+    };
   }
 
   @Bean
   public AuthenticationProvider authenticationProvider() {
+    log.info("Calling authentication provider");
     DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
     authProvider.setUserDetailsService(userDetailsService());
     authProvider.setPasswordEncoder(passwordEncoder());
@@ -36,11 +44,13 @@ public class ApplicationConfig {
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
       throws Exception {
+    log.info("Calling authentication manager");
     return config.getAuthenticationManager();
   }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
+    log.info("Calling password encoder");
     return new BCryptPasswordEncoder();
   }
 
