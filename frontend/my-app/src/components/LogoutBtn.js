3f911f5092d1c4939d6ed59eedc4ec3fc104392e
kmdzinarishvili <ketevan.mdzinarishvili.1@btu.edu.ge>
2023-07-17 19:38:15 +0400

    logout working

--- frontend/my-app/src/components/LogoutBtn.js
+++ frontend/my-app/src/components/LogoutBtn.js
@@ -5,15 +5,12 @@ import axios from 'axios';
 const LOGOUT_URL = '/auth/logout';
 
 const LogoutBtn = () => {
-    const { auth } = useAuth();
+    const navigate = useNavigate();
+    const { auth, setAuth } = useAuth();
     const {REACT_APP_API_BASE_URL:BASE_URL}= process.env;
 
-    console.log("auth", auth)
 
     const logout = async () => {
-        //api call for logout 
-        console.log( "Bearer "+ auth.accessToken);
-        console.log(BASE_URL+LOGOUT_URL)
         const response = await axios.post("/api/v1"+LOGOUT_URL,
             null,
             {
@@ -24,10 +21,10 @@ const LogoutBtn = () => {
                 withCredentials:true
             } 
         );
-        console.log(response)
-        console.log("after resposne")
-        // setAuth({});
-        // navigate('/login');
+        if(response.status ===200 ){
+            setAuth({});
+            navigate('/login');
+        } 
     }
 
     return (
